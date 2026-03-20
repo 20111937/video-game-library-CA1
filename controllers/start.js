438db076b0480008af024be2024e4faf965c017a
20111937 <20111937@mail.wit.ie>
2026-03-20 13:05:31 +0000

    Add files via upload

---
+++ controllers/start.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+
+import logger from "../utils/logger.js";
+import appStore from "../models/app-store.js";
+
+const start = {
+  createView(request, response) {
+    logger.info("Start page loading!");
+    
+    const viewData = {
+     title: "Video Game Library",
+     id: "home"
+      info: appStore.getAppInfo()
+    };
+    
+    response.render('start', viewData);   
+  },
+};
+
+export default start;
