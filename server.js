8a3e646c83d2aade17a24cc2fb4a8e77a660a694
20111937 <20111937@mail.wit.ie>
2026-03-20 13:58:52 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import logger from "./utils/logger.js";
 import { create } from 'express-handlebars';
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
@@ -16,4 +16,4 @@ app.set("view engine", ".hbs");
 
 app.use("/", routes);
 
-app.listen(port, () => logger.info(`Your app is listening on port ${port}`));
+app.listen(PORT, () => logger.info(`Your app is listening on port ${PORT}`));
