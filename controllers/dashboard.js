f482b5ce4fdddc4fd22f98ea82ca3f2f9a4fa741
20111937 <20111937@mail.wit.ie>
2026-05-06 00:54:10 +0100

    Update dashboard.js

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import { v4 as uuidv4 } from "uuid";
 
 import logger from "../utils/logger.js";
 import games from "../models/games.json" with { type: "json" };
@@ -14,8 +15,34 @@ const dashboard = {
       categories: games.categories,
     };
 
-    response.render("dashboard", viewData);
-  },
+   response.render("dashboard", viewData);
+},
+
+addCategory(request, response) {
+  const newCategory = {
+    id: uuidv4(),
+    title: request.body.title,
+    games: []
+  };
+
+  games.categories.push(newCategory);
+
+  response.redirect("/dashboard");
+},
+
+deleteCategory(request, response) {
+  const categoryId = request.params.id;
+
+  const index = games.categories.findIndex(
+    (category) => category.id === categoryId
+  );
+
+  if (index !== -1) {
+    games.categories.splice(index, 1);
+  }
+
+  response.redirect("/dashboard");
+},
 };
 
 export default dashboard;
