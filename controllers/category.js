55e37ba99f68d9fee57068bfbc0850196e60f0c1
20111937 <20111937@mail.wit.ie>
2026-05-06 23:30:28 +0100

    Update category.js

--- controllers/category.js
+++ controllers/category.js
@@ -15,12 +15,12 @@ const category = {
     if (!selectedCategory) {
       return response.status(404).send("Category not found");
     }
-    
-const viewData = {
-  title: selectedCategory.title,
-  id: categoryId,
-  games: selectedCategory.games,
-};
+
+    const viewData = {
+      title: selectedCategory.title,
+      id: categoryId,
+      games: selectedCategory.games,
+    };
 
     response.render("category", viewData);
   },
@@ -40,6 +40,19 @@ const viewData = {
     };
 
     selectedCategory.games.push(newGame);
+
+    response.redirect("/category/" + categoryId);
+  },
+
+  deleteGame(request, response) {
+    const categoryId = request.params.id;
+    const gameIndex = request.params.gameindex;
+
+    const selectedCategory = games.categories.find(
+      (category) => category.id === categoryId
+    );
+
+    selectedCategory.games.splice(gameIndex, 1);
 
     response.redirect("/category/" + categoryId);
   },
