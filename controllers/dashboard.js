09e2889096636a3d67b7353e9e072859d4f7401e
20111937 <20111937@mail.wit.ie>
2026-05-06 01:54:46 +0100

    Update dashboard.js

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -30,20 +30,50 @@ addCategory(request, response) {
 
   response.redirect("/dashboard");
 },
+"use strict";
 
-deleteCategory(request, response) {
+import { v4 as uuidv4 } from "uuid";
+import logger from "../utils/logger.js";
+import games from "../models/games.json" with { type: "json" };
 
-  const categoryId = request.params.id;
+const dashboard = {
+  createView(request, response) {
+    logger.info("Dashboard page loading!");
 
-  const index = games.categories.findIndex(
-    (category) => category.id === categoryId
-  );
+    const viewData = {
+      title: "Game Categories",
+      id: "dashboard",
+      categories: games.categories,
+    };
 
-  if (index !== -1) {
-    games.categories.splice(index, 1);
-  }
+    response.render("dashboard", viewData);
+  },
 
-  response.redirect("/dashboard");
-},
+  addCategory(request, response) {
+    const newCategory = {
+      id: uuidv4(),
+      title: request.body.title,
+      games: [],
+    };
+
+    games.categories.push(newCategory);
+
+    response.redirect("/dashboard");
+  },
+
+  deleteCategory(request, response) {
+    const categoryId = request.params.id;
+
+    const index = games.categories.findIndex(
+      (category) => category.id === categoryId
+    );
+
+    if (index !== -1) {
+      games.categories.splice(index, 1);
+    }
+
+    response.redirect("/dashboard");
+  },
+};
 
 export default dashboard;
