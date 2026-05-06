9f1b6fee42cf2e519a6e9dfac03598af3e517781
20111937 <20111937@mail.wit.ie>
2026-05-06 23:00:49 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,23 +1,22 @@
 'use strict';
+
 import dashboard from "./controllers/dashboard.js";
 import category from "./controllers/category.js";
 import express from 'express';
 import logger from "./utils/logger.js";
 import about from "./controllers/about.js";
+import start from './controllers/start.js';
 
 const router = express.Router();
 
-// add your own routes below
 
-import start from './controllers/start.js';
 router.get('/', start.createView);
 router.get("/dashboard", dashboard.createView);
 router.get("/category/:id", category.createView);
+router.post("/category/:id/addgame", category.addGame);
 router.get("/about", about.createView);
 
 router.post("/dashboard/addcategory", dashboard.addCategory);
 router.get("/dashboard/deletecategory/:id", dashboard.deleteCategory);
-
-
 
 export default router;
