d45e8c824c17be8028247e028aba1d51e829f398
20111937 <20111937@mail.wit.ie>
2026-05-07 00:51:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 'use strict';
 
 import express from 'express';
+import session from "express-session";
 import routes from "./routes.js";
 import logger from "./utils/logger.js";
 import { create } from 'express-handlebars';
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: "videogamelibrarysecret",
+  resave: false,
+  saveUninitialized: true,
+}));
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-const handlebars = create({extname: '.hbs'});
+const handlebars = create({ extname: '.hbs' });
 app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
 
