147168956bb2a3ab8758cd0b3501b7d7269d4371
20111937 <20111937@mail.wit.ie>
2026-05-07 01:32:10 +0100

    Update about.js

--- controllers/about.js
+++ controllers/about.js
@@ -1,7 +1,5 @@
 "use strict";
 
-
-
 import logger from "../utils/logger.js";
 import appInfo from "../models/app-info.json" with { type: "json" };
 import games from "../models/games.json" with { type: "json" };
@@ -11,7 +9,11 @@ const about = {
     logger.info("About page loading!");
 
     const totalCategories = games.categories.length;
-    const totalGames = games.categories.reduce((total, category) => total + category.games.length, 0);
+
+    const totalGames = games.categories.reduce(
+      (total, category) => total + category.games.length,
+      0
+    );
 
     const viewData = {
       title: "About Video Game Library",
@@ -19,11 +21,13 @@ const about = {
       author: "Kurtis Doyle Healy",
       email: "kurtis@gmail.com",
       location: "Ireland",
+      user: request.session.user,
+
       info: {
         ...appInfo,
         categories: totalCategories,
-        games: totalGames
-      }
+        games: totalGames,
+      },
     };
 
     response.render("about", viewData);
